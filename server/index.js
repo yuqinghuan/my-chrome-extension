af4979e8df995ca00141d86c6ccaec9fba601bb3
雨青 <z4ghjdyx@foxmail.com>
2023-03-22 16:08:03 +0800

    feat: 删除无用代码

--- server/index.js
+++ server/index.js
@@ -64,12 +64,3 @@ app.post("/openai", upload.single("image"), async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-
-// [
-//   {
-//       "url": "https://oaidalleapiprodscus.blob.core.windows.net/private/org-tmcbY7exIefYaf4hG4Hps71I/user-Fd1U5NszX5TdsPMU7YbdK0nK/img-PpUfANeFAvgTXYn520Df0KU2.png?st=2023-03-20T04%3A14%3A27Z&se=2023-03-20T06%3A14%3A27Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-03-20T04%3A22%3A25Z&ske=2023-03-21T04%3A22%3A25Z&sks=b&skv=2021-08-06&sig=azkywotF8/aRP9vuKSaqQDHZYvJ6UnQM0zwQlJQ7BB4%3D"
-//   },
-//   {
-//       "url": "https://oaidalleapiprodscus.blob.core.windows.net/private/org-tmcbY7exIefYaf4hG4Hps71I/user-Fd1U5NszX5TdsPMU7YbdK0nK/img-CQNBR4KbskhINmz8PKfFyWzy.png?st=2023-03-20T04%3A14%3A27Z&se=2023-03-20T06%3A14%3A27Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-03-20T04%3A22%3A25Z&ske=2023-03-21T04%3A22%3A25Z&sks=b&skv=2021-08-06&sig=vK8xVaj74aYWnaUsj2sVjLDKBTsoPsQDFX12G9iFxRQ%3D"
-//   }
-// ]
